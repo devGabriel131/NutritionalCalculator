ec4f03e945abefa2a514d8c4329725cc5842dbaa
devGabriel131 <gabriel.velez131@gmail.com>
2026-05-09 22:52:05 -0400

    worked on get nutritional value func and started adding get methods to types

--- NutritionalValue.go
+++ NutritionalValue.go
@@ -10,12 +10,13 @@ const (
 )
 
 type NutritionalData struct {
-	Energy             float64
-	Sugar              float64
-	SaturatedFattyAcid float64
-	Sodium             float64
-	Fiber              float64
-	Protein            float64
+	Energy             EnergyKJ
+	Sugar              SugarGram
+	SaturatedFattyAcid SaturatedFattyAcidsGram
+	Sodium             SodiumMilliGram
+	Fiber              FiberGram
+	Fruit              FruitGram
+	Protein            ProteinGram
 	IsWater            bool
 }
 
@@ -28,6 +29,12 @@ type NutritionalScore struct {
 
 type EnergyKJ float64
 
+func (e EnergyKJ) GetPoints(st ScoreType) int {
+	return
+}
+
+type FruitGram float64
+
 type SugarGram float64
 
 type SaturatedFattyAcidsGram float64
@@ -38,4 +45,31 @@ type FiberGram float64
 
 type ProteinGram float64
 
-func GetNutritionalValue()
+func GetNutritionalValue(n NutritionalData, st ScoreType) NutritionalScore {
+	value := 0
+	positive := 0
+	negative := 0
+
+	if st != Water {
+		// bad most of the time
+		energyPoints := n.Energy.GetPoints(st)
+		sugarPoints := n.Sugar.GetPoints(st)
+		satFatPoints := n.SaturatedFattyAcidsGram.GetPoints(st)
+		sodiumPoints := n.Sodium.GetPoints(st)
+
+		// good most of the time
+		fiberPoints := n.Fiber.GetPoints(st)
+		fruitsPoints := n.Fruit.GetPoints(st)
+		proteinPoints := n.Fiber.GetPoints(st)
+
+		positive += fiberPoints + fruitsPoints + proteinPoints
+		negative += energyPoints + sugarPoints + satFatPoints + sodiumPoints
+	}
+
+	return NutritionalScore{
+		Value:     value,
+		Posititve: positive,
+		Negative:  negative,
+		ScoreType: st,
+	}
+}
